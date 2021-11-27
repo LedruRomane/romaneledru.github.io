0ac2a279fd48e71737da10c073c7ec6307b74d6d
romaneledru <romaneledru@users.noreply.github.com>
2021-11-27 15:55:35 +0000

    deploy: 26cdb2a9af3efcd04d47dd3ccae2737c4e57b8ef

---
+++ _nuxt/aced0ed.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{300:function(e,n,t){"use strict";t.r(n);var l={},c=t(75),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("wesh")])}),[],!1,null,null,null);n.default=component.exports}}]);
